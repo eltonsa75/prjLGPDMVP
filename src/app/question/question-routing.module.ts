bbd7e8fcfed248c22ec531a5a76a5ea58a11f10e
Marcos Vinicius <eltonsa75@hotmail.com>
2020-09-01 11:06:43 -0300

    form-cliente

--- src/app/question/question-routing.module.ts
+++ src/app/question/question-routing.module.ts
@@ -1,3 +1,4 @@
+import { FormClienteComponent } from './FormCliente/form-cliente/form-cliente.component';
 import { ConsultaEntrevistaComponent } from './consulta-entrevista/consulta-entrevista.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -32,8 +33,8 @@ import { AuthGuardService } from './../auth-guard.service';
 
 const routes: Routes = [
   // Tela de Login
-  { path: '', component: LoginComponent}, 
-  { 
+  { path: '', component: LoginComponent},
+  {
     path: 'profile',
     component: ProfileComponent,
     canActivate: [AuthGuardService]
@@ -58,16 +59,17 @@ const routes: Routes = [
   { path: 'painelentrevista',
     component: PainelComponent,
     canActivate: [AuthGuardService]},
-    
-  { path: 'documentalfuncional', component: DocumentalfuncionalComponent}, 
+
+  { path: 'documentalfuncional', component: DocumentalfuncionalComponent},
   { path: 'CicloUmEntrevistaFuncional', component: CicloUmEntrevistadocumentalFuncionalComponent },
-  { path: 'CicloUmDocFundional', component: CicloUmEntrevistaFuncionalComponent }, 
+  { path: 'CicloUmDocFundional', component: CicloUmEntrevistaFuncionalComponent },
   { path: 'CicloUmEntrevistaTecnica', component: CicloUmEntrevistaTecnicaComponent },
   { path: 'CicloUmEntrevistaTecnicaFuncional', component: CicloUmEntrevistaTecnicaFuncionalComponent },
   { path: "ConsultaEntrevista/:id", component: ConsultaEntrevistaComponent},
   { path: 'Selecao-Questionario-Entrevista',
    component: SelecaoQuestionarioEntrevistaComponent,
    canActivate: [AuthGuardService]},
+   { path: 'FormCliente', component: FormClienteComponent},
 
   { path: 'register',
    component: RegisterComponent,
