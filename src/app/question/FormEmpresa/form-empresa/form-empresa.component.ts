4d9889f4bf5ab30babe91d0b8acb7589259efdf6
Marcos Vinicius <eltonsa75@hotmail.com>
2020-09-09 11:03:10 -0300

    FormEmpresa

--- src/app/question/FormEmpresa/form-empresa/form-empresa.component.ts
+++ src/app/question/FormEmpresa/form-empresa/form-empresa.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-form-empresa',
+  selector: 'app-form-cliente',
   templateUrl: './form-empresa.component.html',
   styleUrls: ['./form-empresa.component.css']
 })
 export class FormEmpresaComponent implements OnInit {
+
+  title = "Gestão de Empresa";
+  subTitulo = " Informações do Contato";
 
   constructor() { }
 
